3686eb4b8c92613508c5700197a4719ce08ad305
Raluca Sandu <ralucasanduprisacaru@gmail.com>
2018-10-31 15:55:32 +0000

    user authentication

--- 21. FirebaseTest/app/src/main/java/com/example/raluc/firebasetest/MainActivity.kt
+++ 21. FirebaseTest/app/src/main/java/com/example/raluc/firebasetest/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.raluc.firebasetest
 
+import android.app.Activity
+import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -9,10 +11,12 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import com.example.raluc.firebasetest.databinding.ActivityMainBinding
 import com.example.raluc.firebasetest.utils.ANONYMOUS
 import com.example.raluc.firebasetest.utils.RC_SIGN_IN
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 
@@ -35,6 +39,8 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         initializeRecyclerViewAdapter()
         initializeFirebaseComponents()
+        // authorisation
+        startFirebaseSignIn()
         addTextWatchers()
     }
 
@@ -76,6 +82,24 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         removeReadListeners()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == RC_SIGN_IN && resultCode == Activity.RESULT_OK){
+            firebaseAuth.currentUser?.displayName?.let {
+                onSignedInInitialize(it)
+            }
+            Toast.makeText(this, "Signed in", Toast.LENGTH_LONG).show()
+        }else {
+            val response = IdpResponse.fromResultIntent(data)
+            Toast.makeText(this, response?.providerType, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun onSignedInInitialize(name: String) {
+        userName = name
     }
 
 
